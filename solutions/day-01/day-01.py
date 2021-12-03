13d8e564d546403d4475a1236e747aa63520780f
alexvornsand <ajvornsand@ucdavis.edu>
2021-12-03 10:38:07 -0600

    Update day-01.py

--- solutions/day-01/day-01.py
+++ solutions/day-01/day-01.py
@@ -4,13 +4,15 @@
 # part 1
 numbers = [int(x) for x in open('day-01.txt', 'r').read().split('\n')[:-1]]
 
-def countIncreases(numbers, partTwo = False):
-  if(partTwo == False):
-    print(sum([i2 > i1 for i2, i1 in zip (numbers[1:], numbers[:-1])]))
-  else:
-    print(sum([i2 > i1 for i2, i1 in zip (numbers[3:], numbers[:-3])]))
+
+def countIncreases(numbers, partTwo=False):
+    if(partTwo is False):
+        print(sum([i2 > i1 for i2, i1 in zip(numbers[1:], numbers[:-1])]))
+    else:
+        print(sum([i2 > i1 for i2, i1 in zip(numbers[3:], numbers[:-3])]))
+
 
 countIncreases(numbers)
 
 # part 2
-countIncreases(numbers, partTwo = True)
+countIncreases(numbers, partTwo=True)
